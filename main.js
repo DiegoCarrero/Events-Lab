2d346289f6acd6584e991e4d930e3efce11296dd
Diegocarrero <diegocar2413@gmail.com>
2023-01-15 17:56:45 -0500

    Events Lab

--- main.js
+++ main.js
@@ -7,3 +7,34 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const currentColor = document.getElementById('current-color');
+const paletteColors = document.querySelectorAll('.color');
+
+for (const paletteColor of paletteColors) {
+  paletteColor.addEventListener('click', () => {
+    currentColor.style.background = paletteColor.style.background;
+  })
+}
+
+const cells = document.querySelectorAll('.cell');
+
+for (const cell of cells) {
+  cell.addEventListener('click', () => {
+    cell.style.background = currentColor.style.background;
+  })
+}
+
+const resetButton = document.createElement("button");
+resetButton.setAttribute('type', "button");
+resetButton.style.textAlign = "center";
+resetButton.textContent = "Reset";
+const palette = document.getElementById('palette');
+palette.append(resetButton);
+
+resetButton.addEventListener('click', () => {
+  currentColor.style.background = "white";
+  for (const cell of cells) {
+    cell.style.background = "white";
+  }
+})
